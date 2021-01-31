3310b5cda73db729904fa70cf6a64a20809733ba
ortyomka <iurin.art@gmail.com>
2021-01-31 21:52:52 +0300

    Updated
    Signed-off-by: ortyomka <iurin.art@gmail.com>

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ if __name__ == "__main__":
     files = astor.code_to_ast.find_py_files("./projects")
 
     results = dict()
-    ignore_list = ["load", "store", "del",
-                   "module", "interactive", "expression", "functiontype",
-                   "mod", "stmt", "expr", "expr_context", "boolop",
-                   "operator", "unaryop", "cmpop", "excepthandler",
-                   "arguments", "arg", "keyword", "withitem", "type_ignore",
-                   "name", "attribute", "call",
-                   "importfrom", "import",
-                   "num", "str", "bytes", "nameconstant", "ellipsis"
-                   ]
+    accept_list = ["For", "AsyncFor", "While", "If",
+                   "With", "AsyncWith", "Raise", "Try", "Pass", "Break", "Continue",
+                   "Lambda", "IfExp", "Dict", "Set", "ListComp", "SetComp", "DictComp",
+                   "List", "Tuple", "Slice", "GeneratorExp"]
 
+    error_files = 0
+    all_files = 0
     for i in files:
+        all_files += 1
         try:
             a = astor.code_to_ast.parse_file(i[0] + "/" + i[1])
         except Exception as e:
             print("ERROR", i[0] + "/" + i[1], ":", e)
+            error_files += 1
             continue
 
         for g in ast.walk(a):
-            key = type(g).__name__.capitalize()
-            if key.lower() in ignore_list:
+            key = type(g).__name__
+            if key not in accept_list:
                 continue
 
             if key in results:
@@ -36,3 +35,5 @@ if __name__ == "__main__":
 
     for k in results:
         print(k, ": ", results[k])
+
+    print("From all", all_files, "will reviewed", 1 - (error_files / all_files), "%")
